533462df03e8e72a6f5425c1a85ee9191ffd67b1
Anderson Sanchez <anderson.sanchez@est.ikiam.edu.ec>
2023-02-01 20:23:03 -0500

    Pruebas de quitar el espacio
    me salieron correctas pero al ejecutarlo no

--- coati.py
+++ coati.py
@@ -2,18 +2,21 @@ def faster_downloader():
     """
     Sirve para cargar id_coati.txt en id_coatiy descargar en formato genbank la información correspondiente a los identificadores de accesión usando el ENTREZ de Biopythony se guardar en coati y en coati.gb
     """
-   with open("data/coati.txt") as coati:
-    name = coati.readline()[0:-1]
-    for line in coati:
-        sequence += line.replace("\n","")
+    sequence = ""  
+    with open("data/coati.txt") as coati:
+        name = coati.readline()[0:-1]
+        for line in coati:
+            sequence += line.replace("\n","")
     return faster
 def alignment():
     """
     Sirve para que el algoritmo extraiga solamente las secuencias de la variable coati y realice un alineamiento usando clustalW. El resultado debe ser coati.aln y coati.dnd que deben guardarse en su carpeta de trabajo.
     """
+    
     return alig
 def tree():
     """
     Sirve para que realice el cálculo de las distancias utilizando coati.aln y finalmente que imprima en la pantalla el árbol filogenético y guarde en su carpeta de trabajo el arbol como coati_phylotree.pdf
     """
+    
     return tree
