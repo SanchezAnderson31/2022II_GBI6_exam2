73f8be83996c53b31cb62e82b606423aa5b8ead4
Anderson Sanchez <anderson.sanchez@est.ikiam.edu.ec>
2023-02-01 20:12:25 -0500

    Creacion
    Docstring

--- coati.py
+++ coati.py
@@ -1,16 +1,19 @@
-def fasta_downloader(n,n):
+def faster_downloader():
     """
-    Descarga en formato genbank la información correspondiente a los identificadores de accesión usando el ENTREZ de Biopython 
+    Sirve para cargar id_coati.txt en id_coatiy descargar en formato genbank la información correspondiente a los identificadores de accesión usando el ENTREZ de Biopythony se guardar en coati y en coati.gb
     """
-    from Bio import Entrez  
-        with Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id="KP691608.1") as handle:
-        seq_record = SeqIO.read(handle, "fasta")
-        print("%s with %i features" % (seq_record.id, len(seq_record.features)))
-        seq_record.seq
-    return(fast)
-
-def alignment(): 
-    
-"""    
-para que el algoritmo extraiga solamente las secuencias de la variable coati y realice un alineamiento usando clustalW. El resultado debe ser coati.aln y coati.dnd que deben guardarse en su carpeta de trabajo.
-"""
+   with open("data/coati.txt") as coati:
+    name = coati.readline()[0:-1]
+    for line in coati:
+        sequence += line.replace("\n","")
+    return faster
+def alignment():
+    """
+    Sirve para que el algoritmo extraiga solamente las secuencias de la variable coati y realice un alineamiento usando clustalW. El resultado debe ser coati.aln y coati.dnd que deben guardarse en su carpeta de trabajo.
+    """
+    return alig
+def tree():
+    """
+    Sirve para que realice el cálculo de las distancias utilizando coati.aln y finalmente que imprima en la pantalla el árbol filogenético y guarde en su carpeta de trabajo el arbol como coati_phylotree.pdf
+    """
+    return tree
